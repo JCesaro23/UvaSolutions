dcaab077adc3ea0a7bff53192ceb26782fad5256
JCesaro23 <mamanivillenajc@gmail.com>
2021-02-07 17:25:45 -0400

    Back to the past

---
+++ Uva13025.cpp
@@ -0,0 +1,21 @@
+/*******************************************************
+Name Problem:Back to the past
+Problem Category:adhoc
+Problem Tester: Julio Cesar Mamani Villena.
+Email: mamanivillenajc@gmail.com
+*******************************************************/
+#include<bits/stdc++.h>
+#define PI 2*acos(0.0)
+#define MOD 1000000007
+#define EPS 1e-9
+#define all(x) x.begin(),x.end()
+#define fast(); ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); 
+using namespace std;
+void solve(){
+    printf("May 29, 2013 Wednesday\n");
+}
+int main(){
+    fast();
+    solve();
+    return 0;
+}
