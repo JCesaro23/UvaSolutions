801368ff063d7f364448b89fef36a66ab1932961
Julio Mamani <mamanivillenaj@gmail.com>
2021-02-21 17:48:05 -0400

    intentando hacer un commit

---
+++ template.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define deb(x)	cout<<#x<<" = { "<<x<<" }\n"
+#define fastIO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define forn(i, n) for (int i = 0; i < n; i++)
+#define forall(it,v) for(auto it=v.begin();it!=v.end();++it)
+#define all(a) (a).begin(), (a).end()
+
+int main() {
+    cout<<"hola mundo soy cesaro "<<endl;
+    return 0;
+}
+
